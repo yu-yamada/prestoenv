41c7a675137041a89eb9f3e68b625419aac5f858
yu-yamada <yu.yamada07@gmail.com>
2016-02-12 12:13:57 +0900

    fix some bug.

--- prestoinstall.sh
+++ prestoinstall.sh
@@ -1,19 +1,36 @@
 #!/bin/sh
 
-SCRIPT_DIR=$(cd $(dirname $0);pwd)
+set -u
 
-if [ ! -f $SCRIPT_DIR/prestoenv.conf];then
-  echo "$SCRIPT_DIR/prestoenv.conf is not available" 
+# chk args
+if [ $# -ne 1 ];then
+  echo "usage: ${0##*/} [presto-version]"
   exit 1
 fi
 
-if [ ! -d ${ETC_DIR} ];then
-  echo "etc dir [${ETC_DIR}]  is not available."
-  exit 1;
+PRST_VER=$1
+PRST_URL="https://repo1.maven.org/maven2/com/facebook/presto/presto-server/${PRST_VER}/presto-server-${PRST_VER}.tar.gz"
+
+# chk presto version and url.
+hr=`curl -LI ${PRST_URL} -w '%{http_code}\n' -s -o /dev/null`
+if [ "${hr}" = 404 ]; then
+  echo "VERSION:${PRST_VER} [${PRST_URL}] is not available."
+  exit 1
 fi
 
-PRST_VER=0.112
- . $SCRIPT_DIR/prestoenv.conf
+SCRIPT_DIR=$(cd $(dirname $0);pwd)
+
+if [ ! -f $SCRIPT_DIR/prestoenv.conf ];then
+  echo "$SCRIPT_DIR/prestoenv.conf is not available" 
+  exit 1
+fi
+. $SCRIPT_DIR/prestoenv.conf
+
+if [ ! -d ${ETC_DIR} ];then
+  echo "etc dir [${ETC_DIR}]  is not available."
+  exit 1
+fi
+
 
 if [ ! -e $PRST_BASE_DIR ];then
   mkdir -p $PRST_BASE_DIR
@@ -44,7 +61,7 @@ mkdir -p presto/etc
 cp -r ${ETC_DIR}/* ${PRST_DIR}/etc/
 
 # install cli
-curl -k -O https://repo1.maven.org/maven2/com/facebook/presto/presto-cli/${PTST_VER}/presto-cli-${PRST_VER}-executable.jar
+curl -k -O https://repo1.maven.org/maven2/com/facebook/presto/presto-cli/${PRST_VER}/presto-cli-${PRST_VER}-executable.jar
 
 mv presto-cli-${PRST_VER}-executable.jar ${PRST_DIR/bin/presto
 chmod +x ${PRST_DIR/bin/presto
--- prestolist.sh
+++ prestolist.sh
@@ -3,6 +3,6 @@
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
  . $SCRIPT_DIR/prestoenv.conf
 
-curl -k -s https://repo1.maven.org/maven2/com/facebook/presto/presto-server/ | sed -e "s/<a href=\"\([0-9.]*\)\/.*/\1/g" |grep -e"^[0-9]"
+curl -k -s https://repo1.maven.org/maven2/com/facebook/presto/presto-server/ | sed -e "s/<a href=\"\([0-9.]*\)\/.*/\1/g" |grep -e"^[0-9]" | sort -n -t. -k2,2
 
 
