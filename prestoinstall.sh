9221ac121c2ac0d95b3de8a268bba10799708262
U-yamada <yu.yamada07@gmail.com>
2016-03-16 23:18:23 +0900

    create conf function

--- prestoinstall.sh
+++ prestoinstall.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -u
 
+# $1: args count
 chk_args () {
   if [ $1 -ne 1 ];then
     echo "usage: ${0##*/} [presto-version]"
@@ -9,6 +10,8 @@ chk_args () {
 }
 
 # chk presto version and url.
+# $1:url
+# $2:presto_version
 chk_presto_ver () {
   hr=`curl -LI $1 -w '%{http_code}\n' -s -o /dev/null`
   if [ "${hr}" = 404 ]; then
@@ -27,6 +30,36 @@ load_conf () {
   . $SCRIPT_DIR/prestoenv.conf
 }
 
+# $1:source_dir
+# $2:target_dir
+# $3:file_name
+cp_if_not_exist () {
+  if [ ! -f $2/$3 ];then
+    cp $1/$3 $2/$3
+  fi
+}
+
+
+#create_presto_conf_files
+# $1:etc_dir
+create_presto_conf () {
+  BASE_ETC_DIR=$(cd $(dirname $0);pwd)/etc
+  if [ ! -d $1 ];then
+    mkdir -p $1
+    cp -ir ${BASE_ETC_DIR}/* $1/
+  else
+    cp_if_not_exist ${BASE_ETC_DIR} $1 config.properties
+    cp_if_not_exist ${BASE_ETC_DIR} $1 jvm.config 
+    cp_if_not_exist ${BASE_ETC_DIR} $1 log.properties 
+    cp_if_not_exist ${BASE_ETC_DIR} $1 node.properties 
+    if [ ! -d $1/catalog ];then
+      mkdir $1/catalog
+    fi
+    cp_if_not_exist ${BASE_ETC_DIR}/catalog $1/catalog jmx.properties 
+  fi
+  
+}
+
 chk_args $#
 
 PRST_VER=$1
@@ -38,10 +71,11 @@ chk_presto_ver ${PRST_URL} ${PRST_VER}
 # load setting
 load_conf
 
-if [ ! -d ${ETC_DIR} ];then
-  echo "etc dir [${ETC_DIR}]  is not available."
-  exit 1
-fi
+#create_presto_conf_files
+create_presto_conf ${ETC_DIR}
+
+# TODO
+exit
 
 if [ ! -e $PRST_BASE_DIR ];then
   mkdir -p $PRST_BASE_DIR
